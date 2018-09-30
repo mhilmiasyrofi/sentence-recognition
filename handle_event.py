8f2576e885408e1af7b919344ee8ec2012639510
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2018-09-30 22:10:44 +0200

    limit event, maximun one event for each 2s

--- handle_event.py
+++ handle_event.py
@@ -4,13 +4,27 @@
 import asyncio
 import evdev
 from evdev import InputDevice, categorize, ecodes
+import datetime
+
+last_time = datetime.datetime.now()
 
 devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
 # print(len(devices))
 dev = InputDevice('/dev/input/event' + str(len(devices)-1))
 
+## using sync approach
+for event in dev.read_loop():
+    if event.type == ecodes.EV_KEY:
+        # print(categorize(event))
+        delta = datetime.datetime.now() - last_time
+        combined = delta.seconds + delta.microseconds/1E6
+        # print(combined)
+        if combined > 2 :
+            last_time = datetime.datetime.now()
+            print("event")
 
-## ascync
+
+## using ascync
 # async def helper(dev):
 #     async for ev in dev.async_read_loop():
 #         # print(repr(ev))
@@ -18,9 +32,3 @@ dev = InputDevice('/dev/input/event' + str(len(devices)-1))
 
 # loop = asyncio.get_event_loop()
 # loop.run_until_complete(helper(dev))
-
-## using sync approach
-for event in dev.read_loop():
-    if event.type == ecodes.EV_KEY:
-        # print(categorize(event))
-        print("event")
