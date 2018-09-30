2360335e4151e9a5bed8b9b6b03ee8ec1b03d5bd
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2018-09-30 21:58:54 +0200

    handle event synchrnously

--- handle_event.py
+++ handle_event.py
@@ -2,13 +2,25 @@
 ## Read Event from event17
 
 import asyncio
+import evdev
 from evdev import InputDevice, categorize, ecodes
 
-dev = InputDevice('/dev/input/event17')
+devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
+# print(len(devices))
+dev = InputDevice('/dev/input/event' + str(len(devices)-1))
 
-async def helper(dev):
-    async for ev in dev.async_read_loop():
-        print(repr(ev))
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(helper(dev))
+## ascync
+# async def helper(dev):
+#     async for ev in dev.async_read_loop():
+#         # print(repr(ev))
+#         print('event')
+
+# loop = asyncio.get_event_loop()
+# loop.run_until_complete(helper(dev))
+
+## using sync approach
+for event in dev.read_loop():
+    if event.type == ecodes.EV_KEY:
+        # print(categorize(event))
+        print("event")
