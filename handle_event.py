d0b9a290595619504df5b80f2d8ed3bc6aa6ffb9
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2018-10-01 05:02:09 +0200

    using thread to receive non-blocking input from headset bluetoth

--- handle_event.py
+++ handle_event.py
@@ -7,20 +7,29 @@ from evdev import InputDevice, categorize, ecodes
 import datetime
 
 devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
-# print(len(devices))
 dev = InputDevice('/dev/input/event' + str(len(devices)-1))
 
 import numpy as np
 import cv2
 
-last_time = datetime.datetime.now()
-cap = cv2.VideoCapture(0)
-i = 0
+import _thread
 
-delta = datetime.datetime.now() - last_time
-timer = delta.seconds + delta.microseconds/1E6
-# print(combined)
-while timer < 3 :
+def readInput():
+    last_time = datetime.datetime.now()
+    for event in dev.read_loop():
+        if event.type == ecodes.EV_KEY:
+            # print(categorize(event))
+            delta = datetime.datetime.now() - last_time
+            combined = delta.seconds + delta.microseconds/1E6
+            # print(combined)
+            if combined > 2 :
+                last_time = datetime.datetime.now()
+                print("event")
+
+_thread.start_new_thread( readInput, ( ) )
+
+cap = cv2.VideoCapture(0)
+while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
     # Our operations on the frame come here
@@ -29,52 +38,7 @@ while timer < 3 :
     cv2.imshow('frame', output) 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    delta = datetime.datetime.now() - last_time
-    timer = delta.seconds + delta.microseconds/1E6
-
-
-last_time = datetime.datetime.now()
-
-## using sync approach
-for event in dev.read_loop():
-    if event.type == ecodes.EV_KEY:
-        # print(categorize(event))
-        delta = datetime.datetime.now() - last_time
-        combined = delta.seconds + delta.microseconds/1E6
-        # print(combined)
-        if combined > 2 :
-            last_time = datetime.datetime.now()
-            print("event")
-            # Capture frame-by-frame
-            ret, frame = cap.read()
-            # Our operations on the frame come here
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            output = gray
-            cv2.imshow('frame', output) 
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-    # Display the resulting frame
-    cv2.imshow('frame', output) 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
 
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
-
-# last_time = datetime.datetime.now()
-
-# # using ascync
-# async def helper(dev):
-#     async for ev in dev.async_read_loop():
-#         # print(repr(ev))
-#         delta = datetime.datetime.now() - last_time
-#         combined = delta.seconds + delta.microseconds/1E6
-#         # print(combined)
-#         if combined > 2 :
-#             last_time = datetime.datetime.now()
-#             print('event')
-
-# loop = asyncio.get_event_loop()
-# loop.run_until_complete(helper(dev))
